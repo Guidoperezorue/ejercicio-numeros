706fc79b96fd1e02541175363c13b9d3ab67f6dc
Guidoperezorue <78152166+Guidoperezorue@users.noreply.github.com>
2022-09-16 19:51:43 -0300

    Add files via upload

---
+++ Ejercicio Numeros.js
@@ -0,0 +1,6 @@
+let altura_cm = 173
+let altura_mts = 1.73
+let peso_kg = 63.5
+let altura_round = altura_mts.toPrecision(1)
+let peso_round = parseInt(peso_kg)
+let bool = (Number.MAX_VALUE + 1) === Number.MAX_VALUE
